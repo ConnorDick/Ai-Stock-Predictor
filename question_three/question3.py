f0e39ff193d26805b1254ec28db4c18726134354
ConnorDick <32340160+ConnorDick@users.noreply.github.com>
2022-11-22 21:07:11 -0500

    Update question3.py
    Created numpy arrays for SGDClassifier and generated CLF.

--- question_three/question3.py
+++ question_three/question3.py
@@ -1,32 +1,9 @@
-# import yfinance as yf
-# import pandas as pd
-# import matplotlib as plt
-# from IPython.display import display
-# from tabulate import tabulate
-# import IPython
-#
-# teslaStockTicker = 'TSLA'
-#
-# teslaStockData = yf.Ticker(teslaStockTicker)
-#
-# teslaStockDF = teslaStockData.history(period='1d', start='2022-10-10', end='2022-11-20')
-#
-# column_headers = list(teslaStockDF.columns)
-#
-# teslaStockDF.to_csv('teslaData.csv')
-# print(column_headers)
-# print(teslaStockDF)
-#
-# plt.plot(teslaStockDF)
-#
-# teslaStockDF.plot(teslaStockDF["Date"], teslaStockDF["Close"])
-# display(teslaStockDF)
-
 from sklearn.linear_model import SGDClassifier
 import pandas as pd
 import yfinance as yf
 from datetime import date, timedelta
 from matplotlib import pyplot as plt
+import numpy as np
 
 # calculating the start date
 Start = date.today() - timedelta(40)
@@ -40,13 +17,21 @@ End.strftime('%Y-%m-%d')
 
 # func accepts ticker, return df of date & closing price
 
-
 def closing_price(ticker):
+    testData = []
     asset = pd.DataFrame(yf.download(ticker, start=Start, end=End)['Adj Close'])
-    return asset
+    for index in asset.index:
+        #print(asset['Adj Close'][index])
+        testData.append(asset['Adj Close'][index])
+    return asset, testData
 
-
-TESLA = closing_price('TSLA')                  # CALL THE FUNCTION
+TESLA, testData = closing_price('TSLA')                  # CALL THE FUNCTION
+testData = np.array(testData).reshape(-1, 1)
+#testData.reshape(-1, 1)
+targetValues = []
+for i in range(28):
+    targetValues.append(i)
+print(testData)
 
 # print(TESLA)
 plt.plot(TESLA)
@@ -55,11 +40,8 @@ plt.ylabel('Price ($)')
 plt.xlabel('Date')
 plt.show()
 
-X = [[1], [20], [40]]
-y = [0]
-clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=5)
-clf.fit(X, y)
-SGDClassifier(max_iter=5)
-print(clf.predict[3])
-
-# def stochasticGradientDescent():
+clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=1000)
+clf.fit(testData, targetValues)
+for i in range(90):
+    test = clf.predict([[]])
+    print(test)
